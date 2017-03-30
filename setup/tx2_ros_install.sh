84402d9266b6a72988367a96889b92d0e3a3e977
Dat Nguyen <tdat.nguyen93@gmail.com>
2017-03-29 23:40:06 -0700

    Update tx2_ros_install.sh

--- setup/tx2_ros_install.sh
+++ setup/tx2_ros_install.sh
@@ -4,10 +4,10 @@
 sudo update-locale LANG=C LANGUAGE=C LC_ALL=C LC_MESSAGES=POSIX
 
 # Setup sources.lst
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu trusty main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 
 # Setup keys
-wget https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -O - | sudo apt-key add -
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 
 # Installation
 sudo apt-get update
