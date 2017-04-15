750fafa700e55b148e8f4f283e3defdaadda907c
Dat Nguyen <tdat.nguyen93@gmail.com>
2017-04-14 23:32:13 -0700

    Create install_dependencies.sh

---
+++ setup/install_dependencies.sh
@@ -0,0 +1,17 @@
+
+#!/bin/bash
+# Install Jetson Car Dependencies
+
+# Driver for Teensy 3.2 to be detected on Jetson
+sudo apt-get -y install gcc-avr arduino
+sudo apt-get -y install ros-kinetic-rosserial-arduino ros-kinetic-rosserial ros-kinetic-angles
+
+# Driver for Logitech Joystick
+sudo apt-get -y install ros-kinetic-joy
+
+# The current ROS version of librealsense
+sudo apt-get -y install ros-kinetic-librealsense
+sudo apt-get -y install ros-kinetic-cv-bridge ros-kinetic-cv-camera ros-kinetic-jsk-pcl-ros-utils ros-kinetic-roslint
+# sudo apt-get install ros-indigo-usb-cam -y
+
+# Remove headers /usr/src since they DO NOT match Jetson TX2.
