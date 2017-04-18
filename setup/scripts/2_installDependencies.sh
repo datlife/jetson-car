229e0521535254f627349ce27e5c46bc8e19750d
Dat Nguyen <tdat.nguyen93@gmail.com>
2017-04-18 12:03:12 -0700

    Update 2_installDependencies.sh

--- setup/scripts/2_installDependencies.sh
+++ setup/scripts/2_installDependencies.sh
@@ -2,20 +2,20 @@
 #!/bin/bash
 # Install Jetson Car Dependencies
 
-# Driver for Teensy 3.2 to be detected on Jetson
+# Driver for Teensy 3.2 (Arduino-ish) 
 sudo apt-get -y install gcc-avr arduino
 sudo apt-get -y install ros-kinetic-rosserial-arduino ros-kinetic-rosserial ros-kinetic-angles
 
 # Driver for Logitech Joystick
 sudo apt-get -y install ros-kinetic-joy
 
-# The current ROS version of librealsense
-sudo apt-get -y install ros-kinetic-librealsense
+# Driver for USB Camera node
 sudo apt-get -y install ros-kinetic-cv-bridge ros-kinetic-cv-camera ros-kinetic-jsk-pcl-ros-utils ros-kinetic-roslint
-# sudo apt-get install ros-indigo-usb-cam -y
+sudo apt-get -y install v4l-utils 
 
+# The current ROS version of librealsense
+# sudo apt-get -y install ros-kinetic-librealsense
 # Remove headers /usr/src since they DO NOT match Jetson TX2.
-cd /usr/src
-sudo rm -rf linux-headers-4.4.0-72* uvcvideo-1.1.1-3-realsense/
+# cd /usr/src
+# sudo rm -rf linux-headers-4.4.0-72* uvcvideo-1.1.1-3-realsense/
 
-## SOme libraries for USB cam
