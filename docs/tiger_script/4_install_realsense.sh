304cda01bdda801fe3e2ae0304d95765673351f7
Dat <tdat.nguyen93@gmail.com>
2017-05-04 08:28:03 +0000

    Updated Realsense Installation

--- docs/tiger_script/4_install_realsense.sh
+++ docs/tiger_script/4_install_realsense.sh
@@ -11,22 +11,31 @@ sudo apt-get install libusb-1.0-0-dev pkg-config -y
 sudo apt-get install libglfw3-dev -y
 sudo apt-get install qtcreator -y
 sudo apt-get install cmake -y
+
 # Install librealsense into home directory
 cd $HOME
 git clone https://github.com/IntelRealSense/librealsense.git
 cd librealsense
 # Checkout version 1.11.0 of librealsense, last tested version
 git checkout v1.12.1
+
 # Install Qt libraries
 sudo scripts/install_qt.sh
+
 # Copy over the udev rules so that camera can be run from user space
 sudo cp config/99-realsense-libusb.rules /etc/udev/rules.d/
 sudo udevadm control --reload-rules && udevadm trigger
+
+# Correct libGL path
+cd /usr/lib/aarch64-linux-gnu
+sudo rm libGL.so
+sudo ln -s /usr/lib/aarch64-linux-gnu/tegra/libGL.so libGL.so
+
 # Make and Install
 mkdir build
 cd build
 cmake .. -DBUILD_EXAMPLES:BOOL=true
-make -j4
+sudo make -j6
 sudo make install
 
 
