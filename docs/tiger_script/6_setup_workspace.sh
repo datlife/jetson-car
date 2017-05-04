f6d778cf48e4dc652ac5d036f8941151b41dfed5
Dat Nguyen <tdat.nguyen93@gmail.com>
2017-05-04 02:05:54 -0700

    Update 6_setup_workspace.sh

--- docs/tiger_script/6_setup_workspace.sh
+++ docs/tiger_script/6_setup_workspace.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sudo echo "source export jetson-car/devel/setup.bash" >> ~/.bashrc
+sudo echo "source jetson-car/devel/setup.bash" >> ~/.bashrc
 
 # To communicate to HOST, should be update when using wireless
-export ROS_IP=192.168.1.79
+sudo echo "export ROS_IP=192.168.1.79" >> ~/.bashrc
 
 # Add HOST IP (your laptop/pc)
 sudo echo "192.168.1.78 dat" | sudo tee --append /etc/hosts
